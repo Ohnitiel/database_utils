ec71c684748121aeb571a32fa6c6acaecca59858
Ohnitiel <leitinho.ricardo@gmail.com>
2025-02-14 15:22:23 -0300

    Adicionada documentação

--- queries/delete_key_references.sql
+++ queries/delete_key_references.sql
@@ -1,27 +1,53 @@
--- TODO Add documentation
 CREATE OR REPLACE FUNCTION pg_temp.cascade_delete(
     p_schema_name TEXT,
     p_table_name TEXT,
     p_key_values JSONB
-) RETURNS TEXT AS $$
+) RETURNS TABLE(
+  table_name TEXT,
+  delete_script TEXT,
+  insert_scripts TEXT
+) AS $$
+/*
+    Função: pg_temp.cascade_delete
+    --------------------------------
+    Esta função executa a exclusão em cascata de registros em um banco de dados PostgreSQL.
+    Primeiro, ela identifica e faz backup dos registros afetados, garantindo que todas as
+    tabelas filhas referenciadas por chaves estrangeiras sejam processadas recursivamente.
+    Após o backup, os registros são excluídos seguindo a hierarquia das relações.
+
+    Parâmetros:
+    - p_schema_name: Nome do esquema onde a tabela se encontra.
+    - p_table_name: Nome da tabela de onde a exclusão será iniciada.
+    - p_key_values: Valores da chave primária do registro a ser excluído.
+
+    Exemplos de uso:
+    - SELECT * FROM pg_temp.cascade_delete('public', 'table_name', '{"id": 1}');
+    - SELECT * FROM pg_temp.cascade_delete('public', 'table_name', '{"key": "string"}');
+    - SELECT * FROM pg_temp.cascade_delete('public', 'table_name', '{"key1": 1, "key2": 2}');
+
+    Retorno:
+    - Um comando SQL contendo os INSERTs necessários para restaurar os dados excluídos.
+*/
 DECLARE
-    pk_columns TEXT[];
-    child_pk_columns TEXT[];
-    fk_record RECORD;
-    delete_query TEXT;
-    key_conditions TEXT;
-    backup_sql TEXT := '';
-    row_data TEXT;
-    backup_query TEXT;
-    quoted_table TEXT;
-    child_key_values JSONB;
-    child_key_values_query TEXT;
-    child_key_value JSONB;
-    child_backup_sql TEXT;
-    affected_rows INT;
+    pk_columns              TEXT[];         
+    child_pk_columns        TEXT[];         
+    fk_record               RECORD;         
+    delete_query            TEXT;           
+    key_conditions          TEXT;           
+    backup_sql              TEXT    := '';          
+    row_data                TEXT;           
+    backup_query            TEXT;           
+    quoted_table            TEXT;           
+    child_key_values        JSONB;          
+    child_key_values_query  TEXT;           
+    child_key_value         JSONB;          
+    child_backup_sql        TEXT;           
+    affected_rows           INT;            
 BEGIN
+    -- Garante que o nome da tabela está no formato correto com schema
     quoted_table := quote_ident(p_schema_name) || '.' || quote_ident(p_table_name);
 
+    -- Obtém as colunas da chave primária da tabela alvo
     SELECT array_agg(attname)
     INTO pk_columns
     FROM pg_index i
@@ -31,9 +57,10 @@ BEGIN
     WHERE i.indisprimary AND n.nspname = p_schema_name AND c.relname = p_table_name;
 
     IF pk_columns IS NULL THEN
-        RAISE EXCEPTION 'No primary key found for table %', p_table_name;
+        RAISE EXCEPTION 'Nenhuma chave primária encontrada para a tabela %', p_table_name;
     END IF;
 
+    -- Percorre as tabelas filhas que possuem chaves estrangeiras referenciando esta tabela
     FOR fk_record IN (
           SELECT    quote_ident(tc.table_schema) || '.' || quote_ident(tc.table_name) AS child_table,
                     quote_ident(tc.table_name) AS plain_child_table,
@@ -52,6 +79,8 @@ BEGIN
           AND       ccu.table_name = p_table_name
           GROUP BY  1, 2, 5, kcu.constraint_name
     ) LOOP
+
+        -- Cria a condição WHERE para encontrar registros filhos
         key_conditions := format(
           '(%3$s) IN (
             SELECT *
@@ -59,10 +88,13 @@ BEGIN
           )', p_key_values, fk_record.fk_column_type, array_to_string(fk_record.fk_column, ', ')
         );
 
+        -- Faz backup dos registros filhos antes da exclusão
         backup_query := format('SELECT array_to_json(array_agg(row_to_json(t))) FROM %s t WHERE %s', fk_record.child_table, key_conditions);
         EXECUTE backup_query INTO row_data;
         
+        -- Se existir algum registro filho, executa a exclusão recursivamente
         IF row_data IS NOT NULL THEN
+
             SELECT array_agg(attname)
             INTO child_pk_columns
             FROM pg_index i
@@ -74,33 +106,38 @@ BEGIN
             child_key_values_query := format('SELECT array_to_json(array_agg(row_to_json(t))) FROM (SELECT %s FROM %s WHERE %s) t', array_to_string(child_pk_columns, ', '), fk_record.child_table, key_conditions);
             EXECUTE child_key_values_query INTO child_key_values;
 
+            -- Executa a exclusão recursivamente
             IF child_key_values IS NOT NULL THEN
                 FOR child_key_value IN 
                   EXECUTE format('SELECT * FROM jsonb_array_elements(%L)', child_key_values)
                 LOOP
-                  child_backup_sql := agh.cascade_delete(fk_record.plain_child_table, child_key_value);
-                  backup_sql := child_backup_sql || backup_sql;
+                  RETURN QUERY SELECT * from pg_temp.cascade_delete(p_schema_name, fk_record.plain_child_table, child_key_value);
                 END LOOP;
             END IF;
+
         END IF;
     END LOOP;
 
+    -- Cria a condição WHERE para encontrar o registro principal
     key_conditions := (
         SELECT string_agg(format('%I = %L', col, p_key_values->>col), ' AND ')
         FROM unnest(pk_columns) col
     );
 
+    -- Faz backup do registro principal antes da exclusão
     backup_query := format('SELECT array_to_json(array_agg(row_to_json(t))) FROM %s t WHERE %s', quoted_table, key_conditions);
     EXECUTE backup_query INTO row_data;
     IF row_data IS NOT NULL THEN
         backup_sql := backup_sql || format('INSERT INTO %s SELECT * FROM jsonb_populate_recordset(NULL::%s, %L)' || CHR(10), quoted_table, quoted_table, row_data);
     END IF;
 
-    EXECUTE format('DELETE FROM %s WHERE %s', quoted_table, key_conditions);
+    -- Exclui o registro principal e retorna as linhas afetadas
+    delete_query := format('DELETE FROM %s WHERE %s', quoted_table, key_conditions);
+    EXECUTE delete_query;
     GET DIAGNOSTICS affected_rows = row_count;
     RAISE INFO 'DELETE FROM % WHERE %', quoted_table, key_conditions;
     RAISE INFO 'Deleted % rows', affected_rows;
 
-    RETURN backup_sql;
+    RETURN QUERY SELECT quoted_table, delete_query, backup_sql;
 END;
 $$ LANGUAGE plpgsql;
